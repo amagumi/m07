c8a65b62d4c9af0b74e90314a26d0ec3b1457348
amagumi <amagumi713@gmail.com>
2024-11-05 17:19:29 +0100

    update

---
+++ ecommerce php examen - alba galan - copia/main.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once("com/cart/cart.php");
+include_once("com/user/users.php");
+include_once("com/catalog/catalog.php");
+
+
+
+echo '<a href="tienda.php">Ir a la tienda</a>';
+
+// addToCart(10, 4);
+
+// removeFromCart(5);
+
+// updateCart(12, 10);
+
+// userRegister(3, 'asier', 'chile', '123');
+
+// getCatalog();
+
+// productRegister(12, 'apio', 240, 0.15);
+
+// productExists(4, 20);
+
+// viewCart();
